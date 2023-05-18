3fc93a3aaf31dafaff91a323dd36314250413ae0
Kai Kuller <kavakai@gmail.com>
2023-05-18 13:37:25 -0600

    Add redux context

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,23 @@ import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './scenes/state/index';
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
