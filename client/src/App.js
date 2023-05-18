86b9ef95a5ea507cd63ea040954de6c2834f06d9
Kai Kuller <kavakai@gmail.com>
2023-05-18 11:43:28 -0600

    Build navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import {
   Route,
   useLocation
 } from 'react-router-dom';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
 import Home from './scenes/home/Home';
+import ItemDetails from './scenes/itemDetails/ItemDetails';
 
 const ScrollToTop = () => {
   const { pathename } = useLocation();
@@ -21,9 +25,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='item/:itemId' element={<ItemDetails />} />
+          <Route path='checkout' element={<Checkout />} />
+          <Route path='checkout/success' element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
