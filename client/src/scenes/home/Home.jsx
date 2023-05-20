ab356e02167d2631b62ac2ac529b5f43ed4f2386
Kai Kuller <kavakai@gmail.com>
2023-05-19 18:12:40 -0600

    Build carousel component

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import MainCarousel from './MainCarousel';
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <MainCarousel />
+    </div>
   )
 }
 
