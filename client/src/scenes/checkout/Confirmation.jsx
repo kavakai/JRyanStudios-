b2e3e32b6218b5406aa3c44db58ad491587facb2
Kai Kuller <kavakai@gmail.com>
2023-06-01 12:01:12 -0600

    Add success component to checkout

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Box, Alert, AlertTitle } from '@mui/material';
 
 function Confirmation() {
   return (
-    <div>Confirmation</div>
+    <Box m='90px auto' width='80%' height='50vh'>
+      <Alert severity='success'>
+        <AlertTitle>Success</AlertTitle>
+        You have successfully made an Order - {' '}
+        <strong>Congrats on making your purchase</strong>
+      </Alert>
+    </Box>
   )
 }
 
