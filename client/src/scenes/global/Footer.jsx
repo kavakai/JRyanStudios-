5ee4d586c11c2715b55a81f29aa8de222e30641b
Kai Kuller <kavakai@gmail.com>
2023-08-02 12:04:21 -0600

    Add responsive design to footer

--- client/src/scenes/global/Footer.jsx
+++ client/src/scenes/global/Footer.jsx
@@ -3,13 +3,16 @@ import { useTheme } from '@mui/material';
 import { Box, Typography } from '@mui/material';
 import { shades } from '../../theme';
 import { useNavigate } from 'react-router-dom';
-import logo from '../../logo/logo.png'
+import logo from '../../logo/logo.png';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 function Footer() {
   const { 
     palette: { neutral }
   } = useTheme();
   const navigate = useNavigate();
+  const isNonMobile = useMediaQuery('(min-width: 600px)')
 
   return (
     <Box mt='70px' p='40px 0' backgroundColor={neutral.light}>
@@ -30,12 +33,13 @@ function Footer() {
             mb='30px' 
             color={shades.primary[500]}
             sx={{ cursor: 'pointer' }} 
+            fontSize={isNonMobile ? 'undefined' : '12px'}
           >
             <a style={{ 'text-decoration': 'none', 'color': 'inherit' }} href='#top'>J Ryan Studios</a>
           </Typography>
         </Box>
         <Box>
-          <img src={logo} alt='logo' width='150px' height='150px' style={{ borderRadius: '5px', boxShadow: '5px 5px 5px #c5c2c2' }}/>
+          <img src={logo} alt='logo' width={isNonMobile ? '150px' : '80px'} height={isNonMobile ? '150px' : '80px'} style={{ borderRadius: '5px', boxShadow: '5px 5px 5px #c5c2c2' }}/>
         </Box>
         {/* <Box>
           <Typography variant='h4' fontWeight='bold' mb='30px' sx={{ cursor: 'pointer' }} onClick={() => navigate('/about')}>About Me</Typography>
@@ -52,11 +56,12 @@ function Footer() {
         <Box>
           {/* <Typography variant='h4' mb='30px' sx={{ cursor: 'pointer' }} onClick={() => navigate('/about')}>About Me</Typography> */}
           {/* <Typography variant='h4' mb='30px'>Contact Me</Typography> */}
-          <Typography mb='30px'>Email: jryanreeves@gmail.com</Typography>
-          <Typography mb='30px' >
+          <Typography mb={isNonMobile ? '30px' : '10px'} fontSize={isNonMobile ? 'undefined' : '10px'}>Email: jryanreeves@gmail.com</Typography>
+          <Typography mb={isNonMobile ? '30px' : '10px'} fontSize={isNonMobile ? 'undefined' : '10px'}>
             <a style={{ 'text-decoration': 'none', 'color': 'inherit' }} href='https://www.instagram.com/jryanreeves/?hl=en' rel='noreferrer' target='_blank'>@jryanreeves</a>
           </Typography>
           <Typography
+            fontSize={isNonMobile ? 'undefined' : '10px'}
             variant='h4'   
             color={shades.primary[200]}>
               <a style={{ 'text-decoration': 'none', 'color': 'inherit' }} href='https://www.kai-the-dev.com/' rel='noreferrer' target='_blank'>Website created by: Kai</a>
