5111344f1838c71de52db78a2ea8958ffb8b0d54
Kai Kuller <kavakai@gmail.com>
2023-08-13 08:47:13 -0600

    Troubleshoot images

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -47,6 +47,7 @@ const ShoppingList = () => {
     (item) => item.attributes.category === "placesOfInspiration"
   );
   
+  console.log(items, 'items')
 
   return (
     <Box width="80%" margin="80px auto">
