62c323bbea8acfd1966771b547cef28f75954168
Kai Kuller <kavakai@gmail.com>
2023-08-16 14:38:25 -0600

    Continue stripe issue

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -135,7 +135,7 @@ function Checkout() {
       body: JSON.stringify(requestBody)
     });
     const session = await response.json();
-    console.log(response, 'response')
+    console.log(session.url, 'sesh url')
     await stripe.redirectToCheckout({
       sessionId: session.id,
     })
