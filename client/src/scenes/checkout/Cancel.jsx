e8c88ab58fe6ccb51c3a07331cfc861d26d8e040
Kai Kuller <kavakai@gmail.com>
2023-08-17 10:07:43 -0600

    Update cancel component, change app title

--- client/src/scenes/checkout/Cancel.jsx
+++ client/src/scenes/checkout/Cancel.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Box, Alert, AlertTitle } from '@mui/material';
 
 function Cancel() {
   return (
-    <div>Your Order Did Not Process</div>
+    <Box m='90px auto' width='80%' height='50vh'>
+    <Alert severity='success'>
+      <AlertTitle>Order Canceled</AlertTitle>
+      Something went wrong and your order has been canceled - {' '}
+      <strong>Please try and purchase again, or email us if you have questions</strong>
+    </Alert>
+  </Box>
   )
 }
 
