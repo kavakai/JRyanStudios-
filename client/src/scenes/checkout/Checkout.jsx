1c11312cddd923a027fa6b432086a3c18d4612b0
Kai Kuller <kavakai@gmail.com>
2023-08-17 10:05:06 -0600

    Add cancel checkout component and fix strioe promise issue

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -135,7 +135,7 @@ function Checkout() {
       body: JSON.stringify(requestBody)
     });
     const session = await response.json();
-    console.log(session, 'sesh')
+    console.log(session, 'session')
     console.log(stripe, 'stripe')
     await stripe.redirectToCheckout({
       sessionId: session.id,
