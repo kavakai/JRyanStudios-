9889ca2da065899ccf6c59b7c4614ff6a23a4865
Kai Kuller <kavakai@gmail.com>
2023-08-18 14:16:13 -0600

    Address stripe payment issue

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -135,11 +135,9 @@ function Checkout() {
       body: JSON.stringify(requestBody),
       
     });
-    const session = await response.json();
-    console.log(session, 'session')
-    console.log(response, 'response')
+    // const session = await response.json();
     await stripe.redirectToCheckout({
-      sessionId: session.id,
+      sessionId: response.data.session.id,
     })
   }
 
