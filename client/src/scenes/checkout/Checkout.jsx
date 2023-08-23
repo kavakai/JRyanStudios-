e881168cad12a4e71d616c5a5ae2f877f2c79ead
Kai Kuller <kavakai@gmail.com>
2023-08-23 09:46:23 -0600

    Address 500 error issue

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -53,13 +53,12 @@ const Checkout = () => {
       body: JSON.stringify(requestBody),
     });
     const session = await response.json();
-    await stripe.redirectToCheckout({
-      sessionId: session.id,
-    });
-
     console.log(session, "session");
     console.log(stripe, "stripe");
     console.log(response, "response");
+    await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
   }
 
   return (
