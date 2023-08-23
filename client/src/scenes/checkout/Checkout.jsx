c6e7868ad417f308aac8c13d493ff62a162635f3
Kai Kuller <kavakai@gmail.com>
2023-08-23 11:31:47 -0600

    Add console log to response

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -58,7 +58,7 @@ const Checkout = () => {
     const session = await response.json();
     console.log(session, "session");
     console.log(stripe, "stripe");
-    console.log(response, "response");
+    console.log(response.json(), "response");
     await stripe.redirectToCheckout({
       sessionId: session.id,
     });
