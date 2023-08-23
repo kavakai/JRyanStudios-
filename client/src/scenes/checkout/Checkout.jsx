e0b8528a1f5be7a3c6a91ba8fdf627d255966326
Kai Kuller <kavakai@gmail.com>
2023-08-23 09:11:51 -0600

    Add auth on strapi

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -124,7 +124,7 @@ function Checkout() {
       }))
     };
 
-    const response = await fetch('https://classic-novelty-bafec44cf4.strapiapp.com/api/order', {
+    const response = await fetch('https://classic-novelty-bafec44cf4.strapiapp.com/api/orders', {
       method: 'POST',
       headers: { 
         'Content-Type': 'application/json',
