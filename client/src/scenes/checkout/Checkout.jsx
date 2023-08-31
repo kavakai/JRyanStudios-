42ae1e0d92a442ef2ef3d1282fa0ec11494eefe4
Kai Kuller <kavakai@gmail.com>
2023-08-31 08:50:43 -0600

    Change checkout key

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -30,9 +30,9 @@ const Checkout = () => {
     actions.setTouched({});
   };
   
-  console.log(process.env.CUSTOM_VARIABLES, 'key')
+  console.log(process.env.STRAPI_ADMIN_STRIPE_KEY, 'key')
   const stripePromise = loadStripe('pk_live_51NDsOeGc9Mev9oaLiRxVP47oV3qHuGnP9mTSE2NNIyTBmG7xPZSztxxdcj6bkOE8ZxmEbqJJUVCHCIv1ITcBydK200cY1wrJ99');
-  if(process.env.STRIPE_SECRET_KEY) { 
+  if(process.env.STRAPI_ADMIN_STRIPE_KEY) { 
     console.log('It is set!'); 
   }
   else { 
